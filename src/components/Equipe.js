5bf95e29304bae85ef7513f209a743788893e2cd
Danny Katoro <danielkatoro@gmail.com>
2022-03-20 05:20:14 +0100

    customize the equipe  section

--- src/components/Equipe.js
+++ src/components/Equipe.js
@@ -10,8 +10,8 @@ const Equipe = ()=>{
                     <div>
                         <img src={team1} height="500px" />
                     </div>
-                    <div>
-                        <div  className="pl-2">
+                    <div className="pl-4">
+                        <div  >
                             <h4 className="nav-link2">JACQUES NDAVARO</h4>
                             <h4 className="title2">Coordinateur</h4>
                         </div>
@@ -19,8 +19,22 @@ const Equipe = ()=>{
                             <button className="btn btn-light">←</button>
                             <button className="btn btn-light ml-1">→</button>
                         </div>
-                        <div className="row">
-
+                        <div className="row pt-1">
+                            <div className="team2">
+                                <img src={team1}  />
+                                <p className="title5">JACQUES NDAVARO</p>
+                                <p className="details5">Coordinateur</p>
+                            </div>
+                            <div className="team2">
+                                <img src={team1}  />
+                                <p className="title5">JACQUES NDAVARO</p>
+                                <p className="details5">Coordinateur</p>
+                            </div>
+                            <div className="team2">
+                                <img src={team1}  />
+                                <p className="title5">JACQUES NDAVARO</p>
+                                <p className="details5">Coordinateur</p>
+                            </div>
                         </div>
                     </div>
                 </div>
