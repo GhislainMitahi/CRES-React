63f47c0bbe722b7f6d43433ce446136b4e0b11f5
Danny Katoro <danielkatoro@gmail.com>
2022-03-20 04:33:36 +0100

    adding separator section

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Statistiques from './components/Statistiques';
 import Equipe from './components/Equipe';
 import Equipe2 from './components/Equipe2';
 import Footer from './components/Footer';
+import Separator from './components/Separator';
 import './App.css';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
   <CresObject/>
   <Evenements/>
   <Statistiques/>
+  <Separator/>
   <Equipe/>
   <Equipe2/>
   <Footer/>
---
+++ src/components/Separator.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Separator = ()=>{
+    return (
+        <div className="separator">
+
+        </div>
+    )
+}
+
+export default Separator
