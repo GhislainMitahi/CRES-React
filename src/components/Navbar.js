b9ad51b1fee3a1cdf62a43cedb2642a43b0c7f84
Danny Katoro <danielkatoro@gmail.com>
2022-03-20 02:11:32 +0100

    customize the navbar section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import slack from '../Images/slack.png';
+import cres from '../Images/cres.PNG';
 
 
 const Navbar = ()=>{
@@ -8,7 +8,7 @@ const Navbar = ()=>{
         <div className="navbar1">
             <nav className="navbar navbar-dark">
                <div className="container">
-                <Link className="navbar-brand"><img src={slack} /></Link>
+                <Link className="navbar-brand"><img className="logo" src={cres} /></Link>
                     <div className="navbar1">
                         <button className="btn btn-outline-light my-2 my-sm-0" type="submit">Joindre</button>
                     </div>
