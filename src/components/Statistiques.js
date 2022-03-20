4501e68cff4cb3ac64ca2995df5015cc91109b43
Danny Katoro <danielkatoro@gmail.com>
2022-03-20 04:23:22 +0100

    customize the statistc section

--- src/components/Statistiques.js
+++ src/components/Statistiques.js
@@ -7,52 +7,52 @@ const Statistiques = ()=>{
                 <div className="container pt-5 pb-5">
                     <div className="row">
                         <div className="col-3">
-                            <p>USD</p>
-                            <h2>3 Millard</h2>
+                            <p className="title3">USD</p>
+                            <h2 className="title4">3 Millard</h2>
                             <p>CAPACITE DE FINANCEMENT</p>
                         </div>
                         <div className="col-3">
-                            <p></p>
-                            <h2>4+ ANS</h2>
+                            <p>&nbsp;</p>
+                            <h2 className="title4">4+ ANS</h2>
                             <p>DE SERVICES ET D'EXPERIENCES</p>
                         </div>
                         <div className="col-3">
-                            <p></p>
-                            <h2>20+</h2>
+                            <p>&nbsp;</p>
+                            <h2 className="title4">20+</h2>
                             <p>PARTENAIRES</p>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-3">
                             <p> </p>
-                            <h2>50+ Projets</h2>
+                            <h2 className="title4">50+ Projets</h2>
                             <p>ENCADREMENT DES JEUNES DEVELOPPEMENT DES PRODUITS</p>
                         </div>
                         <div className="col-3">
                             <p> </p>
-                            <h2>200+</h2>
+                            <h2 className="title4">200+</h2>
                             <p>AGENTS PERMANENTS</p>
                         </div>
                         <div className="col-3">
                             <p>USD</p>
-                            <h2>1000+</h2>
+                            <h2 className="title4">1000+</h2>
                             <p>JEUNES FORMES</p>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-3">
-                            <p>USD</p>
-                            <h2>200+</h2>
+                            <p></p>
+                            <h2 className="title4">200+</h2>
                             <p>AGENTS PERMANENTS</p>
                         </div>
                         <div className="col-3">
                             <p></p>
-                            <h2>500</h2>
+                            <h2 className="title4">500</h2>
                             <p>AGENTS SOUS CONTRAT</p>
                         </div>
                         <div className="col-3">
                             <p></p>
-                            <h2>3 Programmes</h2>
+                            <h2 className="title4">3 Programmes</h2>
                             <p>Ujuzi - Pamoja - Mchango</p>
                         </div>
                     </div>
