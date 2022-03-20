d5def4a472621d7e77242b0a1d2675fe51e922c9
Danny Katoro <danielkatoro@gmail.com>
2022-03-20 03:37:15 +0100

    customize the banner section

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,16 +8,16 @@ const Banner = ()=>{
                     <div className="container">
                         <div className="row">
                             <div className="col-6">
-                                <h1>Centre de Recherche et d'Expertise Scientifique</h1>
-                                <p>Une innovation du Programme d'accompagnement des Etudiants Une solution au probleme d'inclusion social de la jeunesse pour la cration et l'implementation de la solution aux problemes de developpement economique et social aux problemes de developpement de la region du Nord-Kivu et de la RDc</p>
+                                <h1 className="title1">Centre de Recherche et d'Expertise Scientifique</h1><br/>
+                                <p className="details1">Une innovation du Programme d'accompagnement des Etudiants Une solution au probleme d'inclusion social de la jeunesse pour la cration et l'implementation de la solution aux problemes de developpement economique et social aux problemes de developpement de la region du Nord-Kivu et de la RDc</p>
                             </div>
                             <div className="col-6">
-                                <ul className="menu2">
-                                    <li className="nav-link">A PROPOS</li>
-                                    <li className="nav-link">HISTOIRE</li>
-                                    <li className="nav-link">PARTENAIRE</li>
-                                    <li className="nav-link">ASSOCIATION</li>
-                                    <li className="nav-link">ADMINISTRATION</li>
+                                <ul className="menu2 offset-8">
+                                    <li className="nav-link "><Link className="nav-link2">A PROPOS</Link></li>
+                                    <li className="nav-link "><Link className="nav-link2">HISTOIRE</Link></li>
+                                    <li className="nav-link "><Link className="nav-link2">PARTENAIRE</Link></li>
+                                    <li className="nav-link "><Link className="nav-link2">ASSOCIATION</Link></li>
+                                    <li className="nav-link "><Link className="nav-link2">ADMINISTRATION</Link></li>
                                 </ul>
                             </div>
                         </div>
