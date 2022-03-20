99e2869a7d036d3c496a62b758318ec10274a1cd
Danny Katoro <danielkatoro@gmail.com>
2022-03-20 04:41:28 +0100

    customize the equipe  section

--- src/components/Equipe.js
+++ src/components/Equipe.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import team1 from "../Images/team1.png";
 
 const Equipe = ()=>{
     return (
-        <div>
-            
+        <div className="equipe">
+            <div className="container">
+                <div className="row">
+                    <div>
+                        <img src={team1} />
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
