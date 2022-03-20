7fb044cc0f34dbcc82575d0d5b9ee0bc57f0a2c5
Danny Katoro <danielkatoro@gmail.com>
2022-03-20 05:30:23 +0100

    customize the equipe2  section

--- src/components/Equipe2.js
+++ src/components/Equipe2.js
@@ -1,16 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import team1 from "../Images/team1.png";
 
-const Equipe2 = ()=>{
-    return (
-        <div className="equipe2">
-            <div className="container">
-                <div>
-                    <p>Le conseil d'administration est elu chaque cinq and par lAssemblee Generale</p>
-                </div>
-
-            </div>
+const Equipe2 = () => {
+  return (
+    <div className="equipe2">
+      <div className="container pt-5 pb-5">
+        <div>
+          <p>
+            Le conseil d'administration est elu chaque cinq and par lAssemblee
+            Generale
+          </p>
         </div>
-    )
-}
-export default Equipe2
+        <div className="row">
+          <div className="team2">
+            <img src={team1} />
+            <p className="title5">Pascal Byumanine</p>
+            <p className="details5">Coordinateur</p>
+          </div>
+          <div className="team2">
+            <img src={team1} />
+            <p className="title5">Pascal Byumanine</p>
+            <p className="details5">Coordinateur</p>
+          </div>
+          <div className="team2">
+            <img src={team1} />
+            <p className="title5">Pascal Byumanine</p>
+            <p className="details5">Coordinateur</p>
+          </div>
+          <div className="team2">
+            <img src={team1} />
+            <p className="title5">Pascal Byumanine</p>
+            <p className="details5">Coordinateur</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Equipe2;
