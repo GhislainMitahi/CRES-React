6697330cf5e941c8a8d423f3d625d4470d83b98f
Danny Katoro <danielkatoro@gmail.com>
2022-03-20 05:37:34 +0100

    customize the footer  section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,15 @@ import { Link } from "react-router-dom";
 
 const Footer = ()=>{
     return (
-        <div>
-
+        <div className="footer">
+            <div className="container pt-3">
+                <div className="text-center text-light">
+                    <h5>CENTRE DE RECHERCHE ET D'EXPERTISE SCIENTIFIQUE</h5>
+                    <div className="col-5">
+                        <hr/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
