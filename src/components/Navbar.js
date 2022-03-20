8fb5f99b5bea6fd0b6cf09f402d7fedb0483833c
Danny Katoro <danielkatoro@gmail.com>
2022-03-20 02:21:04 +0100

    customize the navbar section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import cres from '../Images/cres.PNG';
+import cres from "../Images/cres.PNG";
 
-
-const Navbar = ()=>{
-    return (
-        <div className="navbar1">
-            <nav className="navbar navbar-dark">
-               <div className="container">
-                <Link className="navbar-brand"><img className="logo" src={cres} /></Link>
-                    <div className="navbar1">
-                        <button className="btn btn-outline-light my-2 my-sm-0" type="submit">Joindre</button>
-                    </div>
-               </div>
-                
-            </nav>
+const Navbar = () => {
+  return (
+    <div className="navbar1">
+      <nav className="navbar navbar-dark">
+        <div className="container">
+          <Link className="navbar-brand">
+            <img className="logo" src={cres} />
+          </Link>
+          <div>
+              <Link className="navbar-link nav-link1">Acceuil </Link>
+              <Link className="navbar-link nav-link1">Le CRES </Link>
+              <Link className="navbar-link nav-link1">Innovation </Link>
+              <Link className="navbar-link nav-link1">Opportinutés </Link>
+              <Link className="navbar-link nav-link1">Contact </Link>
+          </div>
+          <div className="navbar1">
+            <button
+              className="btn btn-outline-light my-2 my-sm-0"
+              type="submit"
+            >
+              JOINDRE
+            </button>
+          </div>
         </div>
-    )
-}
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;
