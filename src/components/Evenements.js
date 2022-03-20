8af0cbc2d147ad25a5875f7691548c5501002a3e
Danny Katoro <danielkatoro@gmail.com>
2022-03-20 03:51:20 +0100

    customize the Events section

--- src/components/Evenements.js
+++ src/components/Evenements.js
@@ -1,11 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Evenements = ()=>{
-return (
+const Evenements = () => {
+  return (
     <div className="evenements">
-        lorem
+      <div className="container">
+        <div className="row pt-2">
+          <div className="col-2">
+            <ul className="menu2 ">
+              <li className="nav-link ">
+                <Link className="nav-link2">A PROPOS</Link>
+              </li>
+              <li className="nav-link ">
+                <Link className="nav-link2">HISTOIRE</Link>
+              </li>
+              <li className="nav-link ">
+                <Link className="nav-link2">PARTENAIRE</Link>
+              </li>
+              <li className="nav-link ">
+                <Link className="nav-link2">ASSOCIATION</Link>
+              </li>
+              <li className="nav-link ">
+                <Link className="nav-link2">ADMINISTRATION</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="col-8"></div>
+        </div>
+      </div>
     </div>
-)
-}
-export default Evenements
+  );
+};
+export default Evenements;
