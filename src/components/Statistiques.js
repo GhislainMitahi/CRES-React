b1d06c541cf4fb47fb3713bfb644fee4f5a92deb
Danny Katoro <danielkatoro@gmail.com>
2022-03-20 01:55:43 +0100

    create statistique component

--- src/components/Statistiques.js
+++ src/components/Statistiques.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 const Statistiques = ()=>{
     return (
-        <div>
-            
+        <div className="statistiques">
+                kririiir
         </div>
     )
 }
