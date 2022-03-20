9236b277f2d5ecd0c124622a9977738b7a6c921e
Danny Katoro <danielkatoro@gmail.com>
2022-03-20 04:18:15 +0100

    customize the statistc section

--- src/components/Statistiques.js
+++ src/components/Statistiques.js
@@ -4,53 +4,53 @@ import { Link } from "react-router-dom";
 const Statistiques = ()=>{
     return (
         <div className="statistiques">
-                <div className="container">
+                <div className="container pt-5 pb-5">
                     <div className="row">
-                        <div>
+                        <div className="col-3">
                             <p>USD</p>
                             <h2>3 Millard</h2>
                             <p>CAPACITE DE FINANCEMENT</p>
                         </div>
-                        <div>
+                        <div className="col-3">
                             <p></p>
                             <h2>4+ ANS</h2>
                             <p>DE SERVICES ET D'EXPERIENCES</p>
                         </div>
-                        <div>
+                        <div className="col-3">
                             <p></p>
                             <h2>20+</h2>
                             <p>PARTENAIRES</p>
                         </div>
                     </div>
                     <div className="row">
-                        <div>
+                        <div className="col-3">
                             <p> </p>
                             <h2>50+ Projets</h2>
                             <p>ENCADREMENT DES JEUNES DEVELOPPEMENT DES PRODUITS</p>
                         </div>
-                        <div>
+                        <div className="col-3">
                             <p> </p>
                             <h2>200+</h2>
                             <p>AGENTS PERMANENTS</p>
                         </div>
-                        <div>
+                        <div className="col-3">
                             <p>USD</p>
                             <h2>1000+</h2>
                             <p>JEUNES FORMES</p>
                         </div>
                     </div>
                     <div className="row">
-                        <div>
+                        <div className="col-3">
                             <p>USD</p>
                             <h2>200+</h2>
                             <p>AGENTS PERMANENTS</p>
                         </div>
-                        <div>
+                        <div className="col-3">
                             <p></p>
                             <h2>500</h2>
                             <p>AGENTS SOUS CONTRAT</p>
                         </div>
-                        <div>
+                        <div className="col-3">
                             <p></p>
                             <h2>3 Programmes</h2>
                             <p>Ujuzi - Pamoja - Mchango</p>
